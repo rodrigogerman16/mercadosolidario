0b25c71f11b0b8db37366341f2a5ddba8a56a344
juliilp <105088200+juliilp@users.noreply.github.com>
2023-01-18 16:33:29 -0300

    configuracion de auth

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
