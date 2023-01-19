a9a96fffa64e68f79999517ea8fd144441cd243c
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-01-19 11:08:03 -0300

    create paypal component

--- app/initiatives/[id]/page.jsx
+++ app/initiatives/[id]/page.jsx
@@ -1,9 +1,13 @@
+import Paypal from 'app/Components/Paypal'
 import React from 'react'
 
 const Detail = ({ params }) => {
   const { id } = params
   return (
-    <div>Detail</div>
+    <div>
+      <h1>Detail {id}</h1>
+      <Paypal></Paypal>
+    </div>
   )
 }
 
