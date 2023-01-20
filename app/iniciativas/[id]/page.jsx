02b71414232ea990d2c94853eb714455ba21544a
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-01-19 22:47:40 -0300

    rendering initiative details

--- app/iniciativas/[id]/page.jsx
+++ app/iniciativas/[id]/page.jsx
@@ -1,12 +1,24 @@
-import Paypal from 'app/Components/Paypal'
 import React from 'react'
+import Paypal from 'app/Components/Paypal'
 
-const Detail = ({ params }) => {
+const fetchPosts = () => {
+  return fetch('https://pf-backend-mercadosolidario-production.up.railway.app/posts/posts')
+    .then(res => res.json())
+}
+
+const Detail = async ({ params }) => {
   const { id } = params
+  let post = await fetchPosts()
+  post = post.find(p => p.id == id)
   return (
     <div>
       <h1>Detail {id}</h1>
       <Paypal></Paypal>
+      <div>
+        <h4>{post.title}</h4>
+        <p>{post.description}</p>
+        <span>{post.location}</span>
+      </div>
     </div>
   )
 }
