9fec9d9b034b88a2fdf339ef047384e3c8e4556b
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-01-19 22:47:18 -0300

    rendering cards components for every initiative

--- app/iniciativas/page.jsx
+++ app/iniciativas/page.jsx
@@ -1,8 +1,17 @@
+import Card from 'app/Components/Card'
 import React from 'react'
 
-const Initiative = () => {
+const fetchPosts = () => {
+  return fetch('https://pf-backend-mercadosolidario-production.up.railway.app/posts/posts')
+    .then(res => res.json())
+}
+
+const Initiative = async () => {
+  const posts = await fetchPosts()
   return (
-    <div>Initiative</div>
+    <div>
+      {posts.map(p => <Card id={p.id} key={p.id} title={p.title} description={p.description} location={p.location}></Card>)}
+    </div>
   )
 }
 
