df852ed1f227d55e7c3a43d788315a4e48903e09
Rodrigo German <rodrigoezequielgerman@gmail.com>
2023-01-21 22:35:23 +0100

    Estilado del footer  de negro a rosa clarito

--- app/Components/Footer.jsx
+++ app/Components/Footer.jsx
@@ -5,7 +5,7 @@ import { FiFacebook, FiInstagram } from "react-icons/fi"
 
 export default function Footer(){
     return(
-        <div className="bg-black text-white relative bottom-0 w-screen font-hind">
+        <div className="bg-pink-100 text-black relative bottom-0 w-screen font-hind">
             <div className="flex justify-between m-auto py-12 w-11/12 content-center">
                 <div className="flex flex-col self-center gap-4">
                     <Image src={logo} alt="logo" className="w-32"/>
@@ -15,25 +15,25 @@ export default function Footer(){
                     <div>
                         <span className="font-bold">Contacto:</span>
                         <p className="text-gray-600">Av. Argentina 122, 8300 Neuquen - Argentina</p>
-                        <Link href="#" className="text-pink-300">contacto@mercadosolidario.com</Link>
+                        <Link href="#" className="text-pink-800 font-semibold">contacto@mercadosolidario.com</Link>
                     </div>
                     <div className="flex justify-between">
                         <span className="font-bold self-center">Síguenos:</span>
                         <div className="flex justify-between gap-4 items-center">
-                            <Link href="#" className="text-3xl hover:text-pink-300"><FiInstagram/></Link>
-                            <Link href="#" className="text-3xl hover:text-pink-300"><FiFacebook/></Link>
+                            <Link href="#" className="text-3xl hover:text-pink-700"><FiInstagram/></Link>
+                            <Link href="#" className="text-3xl hover:text-pink-700"><FiFacebook/></Link>
                         </div>
                     </div>
                 </div> 
-                <div className="flex flex-col text-right">
-                    <Link href="#" className="hover:text-pink-300">INICIO</Link>
-                    <Link href="#" className="hover:text-pink-300">INICIATIVAS</Link>
-                    <Link href="#" className="hover:text-pink-300">FAQ</Link>
-                    <Link href="#" className="hover:text-pink-300">ENTRAR</Link>
+                <div className="flex flex-col text-right font-semibold">
+                    <Link href="#" className="hover:text-pink-700">INICIO</Link>
+                    <Link href="#" className="hover:text-pink-700">INICIATIVAS</Link>
+                    <Link href="#" className="hover:text-pink-700">FAQ</Link>
+                    <Link href="#" className="hover:text-pink-700">ENTRAR</Link>
                     <br/>
-                    <Link href="#" className="hover:text-pink-300">QUIENES SOMOS</Link>
-                    <Link href="#" className="hover:text-pink-300">QUE HACEMOS</Link>
-                    <Link href="#" className="hover:text-pink-300">ULTIMAS NOTICIAS</Link>
+                    <Link href="#" className="hover:text-pink-700">QUIENES SOMOS</Link>
+                    <Link href="#" className="hover:text-pink-700">QUE HACEMOS</Link>
+                    <Link href="#" className="hover:text-pink-700">ULTIMAS NOTICIAS</Link>
                 </div>
             </div>
 
@@ -42,11 +42,11 @@ export default function Footer(){
             <div className="flex justify-between m-auto w-11/12 py-6">
                 <div className="flex gap-4 text-gray-600">
                     <span>@2023 Mercado Solidario - Organizacion sin fines de lucro</span>
-                    <Link href="#" className="hover:text-pink-300">Política de privacidad</Link>
-                    <Link href="#" className="hover:text-pink-300">Términos y condiciones</Link>
+                    <Link href="#" className="hover:text-pink-700">Política de privacidad</Link>
+                    <Link href="#" className="hover:text-pink-700">Términos y condiciones</Link>
                 </div>
                 <div>
-                    <Link href="#" className="hover:text-pink-300">Creditos</Link>
+                    <Link href="#" className="hover:text-pink-700">Creditos</Link>
                 </div>
 
             </div>
