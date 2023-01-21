053000c3874cd7d80620dfcdbf4ddf95e5a48bfe
Rodrigo German <rodrigoezequielgerman@gmail.com>
2023-01-21 21:30:20 +0100

    configuracion en layout para flowbite

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,12 +10,15 @@ export default function RootLayout({ children }) {
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head>
+      <script src="https://cdn.tailwindcss.com"></script>
       <link href="https://fonts.googleapis.com/css2?family=Hind+Madurai:wght@300&family=Lora:ital@1&family=Montserrat:wght@700&display=swap" rel="stylesheet"/>
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.2/flowbite.min.css"  rel="stylesheet" />
       </head>
       <body>
         <Navbar/>
         {children}
         <Footer/>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.2/flowbite.min.js"></script>
       </body>
     </html>
   )
