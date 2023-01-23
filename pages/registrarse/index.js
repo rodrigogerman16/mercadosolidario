b5e730277c0acffd5df04db07db7ac76c32116c0
juliilp <105088200+juliilp@users.noreply.github.com>
2023-01-23 11:09:33 -0300

    Email correccion

--- pages/registrarse/index.js
+++ pages/registrarse/index.js
@@ -1,7 +1,7 @@
 'use client'
 import Link from "next/link";
 import { useState } from "react";
-import emailPost from "./emailPost";
+import emailPost from "@/Components/emailPost";
 function Validate(input) {
   let errors = {};
   if (/[`!@#$%^&*()_+\-=[\]{};':"\\|,.<>\d/?~]/.test(input.name)) {
