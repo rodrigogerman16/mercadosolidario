f5e125fd75cf3d5eecb867e4ef11c5be9570e9bc
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-01-22 23:59:02 -0300

    Agregando Nav y Footer en todas las rutas

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import Footer from '@/Components/Footer'
+import Navbar from '@/Components/Navbar'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div>
+    <Navbar></Navbar>
+    <Component {...pageProps} />
+    <Footer></Footer>
+  </div>
 }
