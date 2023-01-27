18a4b537cdaec06e7b7a9f6acbfc50c510f7ca53
Rodrigo German <rodrigoezequielgerman@gmail.com>
2023-01-27 03:40:59 +0100

    test para auth

--- pages/ingresar/index.js
+++ pages/ingresar/index.js
@@ -2,23 +2,14 @@
 import { useState } from "react";
 import Link from "next/link";
 import axios from "axios";
-import {useSession, signIn, signOut} from "next-auth/react"
+import {useSession, signIn, signOut, getSession} from "next-auth/react"
+import Image from "next/image";
 
 export default function Login() {
   const {data: session} = useSession()
 
-  {/*If session - dentro 3 if - if session.user.name === admin return redirect a page dashboard, if session.user.algo === ong/gptw return redirect a /o (ong), if session.user.algo === usuario redirect a /u (usuario) */}
-  if(session){
+  if(!session){   
     return (
-      <div>
-        <p>welcome, {session.user.email}</p>
-        <button onClick={() => signOut()}>signOut</button>
-      </div>
-    )
-  }
-
-  else{
-  return (
     <div className="w-full max-w-md p-4 rounded-md sm:p-8 m-auto min-h-[calc(100vh-100px)] flex flex-col justify-center items-center">
       <h2 className="mb-3 text-3xl font-semibold text-center">Ingresa a tu cuenta</h2>
       <p className="text-sm text-center dark:text-gray-400">No tienes cuenta?&nbsp;
@@ -46,7 +37,7 @@ export default function Login() {
         </div>
       </form>
       <div className="my-6 space-y-4 w-full">
-        <button aria-label="Login with Google" type="button" className="flex items-center justify-center w-full p-4 space-x-4 border shadow focus:ring-0 w-full rounded">
+        <button onClick={() => signIn("api/auth/signin/google")} aria-label="Login with Google" type="button" className="flex items-center justify-center w-full p-4 space-x-4 border shadow focus:ring-0 w-full rounded">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-pink-400">
             <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
           </svg>
@@ -54,5 +45,16 @@ export default function Login() {
         </button>
       </div>
     </div>    
-  )};
-}
+  )
+  }
+
+  else{
+    return(
+      <div>
+        <p>{session.user.name}</p>
+        <Image src={session.user.image} width={50} height={50} className={"rounded-3xl"} alt=""/>
+        <button onClick= {(e) => signOut(e)}>Sign Out</button>
+      </div>
+    )
+  }
+};  
--- pages/usuario/index.js
+++ pages/usuario/index.js
@@ -1,33 +1,29 @@
 import React from "react";
-import { useSession, signOut, getSession } from "next-auth/react";
+import {signOut, getSession } from "next-auth/react";
 import Image from "next/image";
 
-const Account = () =>{
-
-    const {data: session, status} = useSession()
-   
-
-    if(status === 'authenticated'){
+const Account = ({session}) =>{
+    const {user} = session;
         return(
             <div>
-                <div>Hola {session.user.name}</div>
-                <Image src={session.user.image} width={50} height={50} className={"rounded-3xl"} alt=""/>                
+                <div>Hola {user.name}</div>
+                <Image src={user.image} width={50} height={50} className={"rounded-3xl"} alt=""/>                
                 <button onClick={() => signOut()}>signOut</button>
             </div>
         );
-    }
-    else{
-        return(
-            <div>No estas logeado</div>
-        );
-    }
 }
 
 export default Account;
 
 export const getServerSideProps = async(context) => {
     const session = await getSession(context)
-
+    if(!session){
+        return {
+            redirect: {
+                destination: '/ingresar',
+            },
+        };
+    }
     return{
         props:{session},
     }
