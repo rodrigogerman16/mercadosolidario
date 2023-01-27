9876cc342fa03e1d3df9b0729723396b5f9d8abb
juliilp <105088200+juliilp@users.noreply.github.com>
2023-01-26 21:20:34 -0300

    estilos bauti + render

--- pages/iniciativas/index.js
+++ pages/iniciativas/index.js
@@ -1,73 +1,88 @@
-import React, { Fragment, useEffect, useState } from 'react'
-import { Dialog, Disclosure, Menu, Transition } from '@headlessui/react'
-import { BsChevronDown, BsFunnelFill, BsDash, BsPlus, BsGridFill, BsX } from 'react-icons/bs'
-import Link from 'next/link'
-import Card from '../../Components/Card'
+import React, { Fragment, useEffect, useState } from "react";
+import { Dialog, Disclosure, Menu, Transition } from "@headlessui/react";
+import {
+  BsChevronDown,
+  BsFunnelFill,
+  BsDash,
+  BsPlus,
+  BsGridFill,
+  BsX,
+} from "react-icons/bs";
+import Link from "next/link";
+import Card from "../../Components/Card";
 
 const sortOptions = [
+  { name: "Titulo Asc", href: "#", current: true },
+  { name: "Titulo Desc", href: "#", current: false },
+  { name: "Fecha Asc", href: "#", current: false },
+  { name: "Fecha Desc", href: "#", current: false },
+];
 
-
-  { name: 'Most Popular', href: '#', current: true },
-  { name: 'Best Rating', href: '#', current: false },
-  { name: 'Newest', href: '#', current: false },
-  { name: 'Price: Low to High', href: '#', current: false },
-  { name: 'Price: High to Low', href: '#', current: false },
-]
-const subCategories = [
-
-
-  { name: 'Totes', href: '#' },
-  { name: 'Backpacks', href: '#' },
-  { name: 'Travel Bags', href: '#' },
-  { name: 'Hip Bags', href: '#' },
-  { name: 'Laptop Sleeves', href: '#' },
-]
 const filters = [
-
-
   {
-    id: 'color',
-    name: 'Color',
+    id: "color",
+    name: "Color",
     options: [
-      { value: 'white', label: 'White', checked: false },
-      { value: 'beige', label: 'Beige', checked: false },
-      { value: 'blue', label: 'Blue', checked: true },
-      { value: 'brown', label: 'Brown', checked: false },
-      { value: 'green', label: 'Green', checked: false },
-      { value: 'purple', label: 'Purple', checked: false },
+      { value: "Buenos Aires", label: "Buenos Aires", checked: false },
+      { value: "Catamarca", label: "Catamarca", checked: false },
+      { value: "Chaco", label: "Chaco", checked: true },
+      { value: "Chubut", label: "Chubut", checked: false },
+      { value: "Córdoba", label: "Córdoba", checked: false },
+      { value: "Corrientes", label: "Corrientes", checked: false },
+      { value: "Entre Ríos", label: "Entre Ríos", checked: false },
+      { value: "Formosa", label: "Formosa", checked: false },
+      { value: "Jujuy", label: "Jujuy", checked: true },
+      { value: "La Pampa", label: "La Pampa", checked: false },
+      { value: "La Rioja", label: "La Rioja", checked: false },
+      { value: "Mendoza", label: "Mendoza", checked: false },
+      { value: "Misiones", label: "Misiones", checked: false },
+      { value: "Neuquén", label: "Neuquén", checked: false },
+      { value: "Río Negro", label: "Río Negro", checked: true },
+      { value: "Salta", label: "Salta", checked: false },
+      { value: "San Juan", label: "San Juan", checked: false },
+      { value: "San Luis", label: "San Luis", checked: false },
+      { value: "Santa Cruz", label: "Santa Cruz", checked: false },
+      { value: "Santa Fé", label: "Santa Fé", checked: false },
+      {
+        value: "Santiago del Estero",
+        label: "Santiago del Estero",
+        checked: true,
+      },
+      { value: "Tierra del Fuego", label: "Tierra del Fuego", checked: false },
+      { value: "Tucuman", label: "Tucuman", checked: false },
     ],
   },
   {
-    id: 'category',
-    name: 'Category',
+    id: "category",
+    name: "Category",
     options: [
-      { value: 'new-arrivals', label: 'New Arrivals', checked: false },
-      { value: 'sale', label: 'Sale', checked: false },
-      { value: 'travel', label: 'Travel', checked: true },
-      { value: 'organization', label: 'Organization', checked: false },
-      { value: 'accessories', label: 'Accessories', checked: false },
+      { value: "new-arrivals", label: "New Arrivals", checked: false },
+      { value: "sale", label: "Sale", checked: false },
+      { value: "travel", label: "Travel", checked: true },
+      { value: "organization", label: "Organization", checked: false },
+      { value: "accessories", label: "Accessories", checked: false },
     ],
   },
   {
-    id: 'size',
-    name: 'Size',
+    id: "size",
+    name: "Size",
     options: [
-      { value: '2l', label: '2L', checked: false },
-      { value: '6l', label: '6L', checked: false },
-      { value: '12l', label: '12L', checked: false },
-      { value: '18l', label: '18L', checked: false },
-      { value: '20l', label: '20L', checked: false },
-      { value: '40l', label: '40L', checked: true },
+      { value: "2l", label: "2L", checked: false },
+      { value: "6l", label: "6L", checked: false },
+      { value: "12l", label: "12L", checked: false },
+      { value: "18l", label: "18L", checked: false },
+      { value: "20l", label: "20L", checked: false },
+      { value: "40l", label: "40L", checked: true },
     ],
   },
-]
+];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function Products({ data }) {
-  const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false)
+  const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
 
   const [hydrated, setHydrated] = React.useState(false);
   useEffect(() => {
@@ -76,7 +91,6 @@ export default function Products({ data }) {
   if (!hydrated) {
     return null;
   }
-
 
   /* 
   ANTERIOR
@@ -185,7 +199,11 @@ export default function Products({ data }) {
       <div>
         {/* Mobile filter dialog */}
         <Transition.Root show={mobileFiltersOpen} as={Fragment}>
-          <Dialog as="div" className="relative z-40 lg:hidden" onClose={setMobileFiltersOpen}>
+          <Dialog
+            as="div"
+            className="relative z-40 lg:hidden"
+            onClose={setMobileFiltersOpen}
+          >
             <Transition.Child
               as={Fragment}
               enter="transition-opacity ease-linear duration-300"
@@ -210,7 +228,9 @@ export default function Products({ data }) {
               >
                 <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-12 shadow-xl">
                   <div className="flex items-center justify-between px-4">
-                    <h2 className="text-lg font-medium text-gray-900">Filters</h2>
+                    <h2 className="text-lg font-medium text-gray-900">
+                      Filters
+                    </h2>
                     <button
                       type="button"
                       className="-mr-2 flex h-10 w-10 items-center justify-center rounded-md bg-white p-2 text-gray-400"
@@ -224,7 +244,10 @@ export default function Products({ data }) {
                   {/* Filters */}
                   <form className="mt-4 border-t border-gray-200">
                     <h3 className="sr-only">Categories</h3>
-                    <ul role="list" className="px-2 py-3 font-medium text-gray-900">
+                    <ul
+                      role="list"
+                      className="px-2 py-3 font-medium text-gray-900"
+                    >
                       {subCategories.map((category) => (
                         <li key={category.name}>
                           <a href={category.href} className="block px-2 py-3">
@@ -235,17 +258,29 @@ export default function Products({ data }) {
                     </ul>
 
                     {filters.map((section) => (
-                      <Disclosure as="div" key={section.id} className="border-t border-gray-200 px-4 py-6">
+                      <Disclosure
+                        as="div"
+                        key={section.id}
+                        className="border-t border-gray-200 px-4 py-6"
+                      >
                         {({ open }) => (
                           <>
                             <h3 className="-mx-2 -my-3 flow-root">
                               <Disclosure.Button className="flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-gray-500">
-                                <span className="font-medium text-gray-900">{section.name}</span>
+                                <span className="font-medium text-gray-900">
+                                  {section.name}
+                                </span>
                                 <span className="ml-6 flex items-center">
                                   {open ? (
-                                    <BsDash className="h-5 w-5" aria-hidden="true" />
+                                    <BsDash
+                                      className="h-5 w-5"
+                                      aria-hidden="true"
+                                    />
                                   ) : (
-                                    <BsX className="h-5 w-5" aria-hidden="true" />
+                                    <BsX
+                                      className="h-5 w-5"
+                                      aria-hidden="true"
+                                    />
                                   )}
                                 </span>
                               </Disclosure.Button>
@@ -253,7 +288,10 @@ export default function Products({ data }) {
                             <Disclosure.Panel className="pt-6">
                               <div className="space-y-6">
                                 {section.options.map((option, optionIdx) => (
-                                  <div key={option.value} className="flex items-center">
+                                  <div
+                                    key={option.value}
+                                    className="flex items-center"
+                                  >
                                     <input
                                       id={`filter-mobile-${section.id}-${optionIdx}`}
                                       name={`${section.id}[]`}
@@ -285,9 +323,15 @@ export default function Products({ data }) {
 
         <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="flex items-baseline justify-between border-b border-gray-200 pt-24 pb-6">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900">Iniciativas</h1>
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900">
+              Iniciativas
+            </h1>
 
-            <input type={'search'} className='rounded shadow' placeholder='Buscar...'></input>
+            <input
+              type={"search"}
+              className="rounded shadow"
+              placeholder="Buscar..."
+            ></input>
 
             <div className="flex items-center">
               <Menu as="div" className="relative inline-block text-left">
@@ -318,9 +362,11 @@ export default function Products({ data }) {
                             <a
                               href={option.href}
                               className={classNames(
-                                option.current ? 'font-medium text-gray-900' : 'text-gray-500',
-                                active ? 'bg-gray-100' : '',
-                                'block px-4 py-2 text-sm'
+                                option.current
+                                  ? "font-medium text-gray-900"
+                                  : "text-gray-500",
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm"
                               )}
                             >
                               {option.name}
@@ -333,7 +379,10 @@ export default function Products({ data }) {
                 </Transition>
               </Menu>
 
-              <button type="button" className="-m-2 ml-5 p-2 text-gray-400 hover:text-gray-500 sm:ml-7">
+              <button
+                type="button"
+                className="-m-2 ml-5 p-2 text-gray-400 hover:text-gray-500 sm:ml-7"
+              >
                 <span className="sr-only">View grid</span>
                 <BsGridFill className="h-5 w-5" aria-hidden="true" />
               </button>
@@ -357,7 +406,10 @@ export default function Products({ data }) {
               {/* Filters */}
               <form className="hidden lg:block">
                 <h3 className="sr-only">Categories</h3>
-                <ul role="list" className="space-y-4 border-b border-gray-200 pb-6 text-sm font-medium text-gray-900">
+                <ul
+                  role="list"
+                  className="space-y-4 border-b border-gray-200 pb-6 text-sm font-medium text-gray-900"
+                >
                   {subCategories.map((category) => (
                     <li key={category.name}>
                       <a href={category.href}>{category.name}</a>
@@ -366,15 +418,24 @@ export default function Products({ data }) {
                 </ul>
 
                 {filters.map((section) => (
-                  <Disclosure as="div" key={section.id} className="border-b border-gray-200 py-6">
+                  <Disclosure
+                    as="div"
+                    key={section.id}
+                    className="border-b border-gray-200 py-6"
+                  >
                     {({ open }) => (
                       <>
                         <h3 className="-my-3 flow-root">
                           <Disclosure.Button className="flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
-                            <span className="font-medium text-gray-900">{section.name}</span>
+                            <span className="font-medium text-gray-900">
+                              {section.name}
+                            </span>
                             <span className="ml-6 flex items-center">
                               {open ? (
-                                <BsDash className="h-5 w-5" aria-hidden="true" />
+                                <BsDash
+                                  className="h-5 w-5"
+                                  aria-hidden="true"
+                                />
                               ) : (
                                 <BsX className="h-5 w-5" aria-hidden="true" />
                               )}
@@ -384,7 +445,10 @@ export default function Products({ data }) {
                         <Disclosure.Panel className="pt-6">
                           <div className="space-y-4">
                             {section.options.map((option, optionIdx) => (
-                              <div key={option.value} className="flex items-center">
+                              <div
+                                key={option.value}
+                                className="flex items-center"
+                              >
                                 <input
                                   id={`filter-${section.id}-${optionIdx}`}
                                   name={`${section.id}[]`}
@@ -413,18 +477,22 @@ export default function Products({ data }) {
               <div className="grid w-full col-span-3 sm:grid-cols-2 xl:grid-cols-3 gap-4">
                 {data.length !== 0
                   ? data.map((e) => (
-                    <Link className='w-full' key={e.id} href={`/iniciativas/${e.id}`}>
-                      <Card
+                      <Link
+                        className="w-full"
                         key={e.id}
-                        title={e.title}
-                        image={e.image}
-                        description={e.description}
-                        location={e.location}
-                        isVolunteer={e.type_of_volunteer}
-                        expirationDate={e.expirationDate}
-                      />
-                    </Link>
-                  ))
+                        href={`/iniciativas/${e.id}`}
+                      >
+                        <Card
+                          key={e.id}
+                          title={e.title}
+                          image={e.image}
+                          description={e.description}
+                          location={e.location}
+                          isVolunteer={e.type_of_volunteer}
+                          expirationDate={e.expirationDate}
+                        />
+                      </Link>
+                    ))
                   : "No hay cartas para mostrar"}
               </div>
             </div>
@@ -432,7 +500,7 @@ export default function Products({ data }) {
         </main>
       </div>
     </div>
-  )
+  );
 }
 
 export function getStaticProps() {
