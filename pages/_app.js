fc474686e7b1a4fecff7736cae2a23f5172f3773
Rodrigo German <rodrigoezequielgerman@gmail.com>
2023-01-30 16:31:23 +0100

    typo

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
-import Footer from '@/Components/Footer'
-import Navbar from '@/Components/Navbar'
-import '@/styles/globals.css'
-import { SessionProvider } from 'next-auth/react'
+import Footer from "@/Components/Footer";
+import Navbar from "@/Components/Navbar";
+import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps, session }) {
-  return(
-    <SessionProvider session={session}>
-      <Navbar/>
-      <Component {...pageProps} />
-      <Footer/>
-    </SessionProvider>
-  )
+  return (
+      <SessionProvider session={session}>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </SessionProvider>
+  );
 }
