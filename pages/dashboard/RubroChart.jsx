72bb9985e3ae46933e36a6597a351e57051dba83
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-02-02 14:47:08 -0300

    Eliminando hardcodeo del chart rubros

--- pages/dashboard/RubroChart.jsx
+++ pages/dashboard/RubroChart.jsx
@@ -2,89 +2,88 @@ import React, { PureComponent } from 'react';
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 export default class Example extends PureComponent {
-  static demoUrl = 'https://codesandbox.io/s/bar-chart-with-customized-event-4k1bd';
 
   state = {
     data: [
       {
         name: 'Alimentacion',
-        val: 4000,
+        val: this.props.posts.filter(p => p.rubro == 'Alimentacion').length,
       },
       {
         name: 'Asesoria_legal',
-        val: 3000,
+        val: this.props.posts.filter(p => p.rubro == 'Asesoria_legal').length,
       },
       {
         name: 'Ayuda_a_refugiados',
-        val: 2000,
+        val: this.props.posts.filter(p => p.rubro == 'Ayuda_a_refugiados').length,
       },
       {
         name: 'Apoyo_a_comunidades_indigenas',
-        val: 2780,
+        val: this.props.posts.filter(p => p.rubro == 'Apoyo_a_comunidades_indigenas').length,
       },
       {
         name: 'Apoyo_a_lgbt',
-        val: 1890,
+        val: this.props.posts.filter(p => p.rubro == 'Apoyo_a_lgbt').length,
       },
       {
         name: 'Apoyo_a_la_mujer',
-        val: 2390,
+        val: this.props.posts.filter(p => p.rubro == 'Apoyo_a_la_mujer').length,
       },
       {
         name: 'Construccion_obras',
-        val: 3490,
+        val: this.props.posts.filter(p => p.rubro == 'Construccion_obras').length,
       },
       {
         name: 'Cultura',
-        val: 4000,
+        val: this.props.posts.filter(p => p.rubro == 'Cultura').length,
       },
       {
         name: 'Deportes',
-        val: 3000,
+        val: this.props.posts.filter(p => p.rubro == 'Deportes').length,
       },
       {
         name: 'Derechos_humanos',
-        val: 2000,
+        val: this.props.posts.filter(p => p.rubro == 'Derechos_humanos').length,
       },
       {
         name: 'Discapacitados',
-        val: 2780,
+        val: this.props.posts.filter(p => p.rubro == 'Discapacitados').length,
       },
       {
         name: 'Educacion',
-        val: 1890,
+        val: this.props.posts.filter(p => p.rubro == 'Educacion').length,
       },
       {
         name: 'Medio_ambiente',
-        val: 2390,
+        val: this.props.posts.filter(p => p.rubro == 'Medio_ambiente').length,
       },
       {
         name: 'Entretenimiento',
-        val: 3490,
+        val: this.props.posts.filter(p => p.rubro == 'Entretenimiento').length,
       },
       {
         name: 'Gobierno_no_lucro',
-        val: 2780,
+        val: this.props.posts.filter(p => p.rubro == 'Gobierno_no_lucro').length,
       },
       {
         name: 'Materia_prima',
-        val: 1890,
+        val: this.props.posts.filter(p => p.rubro == 'Materia_prima').length,
       },
       {
         name: 'Medios_de_comunicacion',
-        val: 2390,
+        val: this.props.posts.filter(p => p.rubro == 'Medios_de_comunicacion').length,
       },
       {
         name: 'Salud_medicina',
-        val: 3490,
+        val: this.props.posts.filter(p => p.rubro == 'Salud_medicina').length,
       },
       {
         name: 'Servicio_comunitario',
-        val: 2780,
+        val: this.props.posts.filter(p => p.rubro == 'Servicio_comunitario').length,
       },
       {
         name: 'Transporte',
-        val: 2000,
+        val: this.props.posts.filter(p => p.rubro == 'Transporte').length,
       },
     ],
     activeIndex: 0,
