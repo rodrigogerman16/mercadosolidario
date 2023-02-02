b8755f9b2504300672d991e1e2995f3292d4b6a8
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-02-02 11:37:11 -0300

    Nuevos estilos Rechart

--- Components/Rechart.jsx
+++ Components/Rechart.jsx
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { AreaChart, Area, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
 const date = new Date()
@@ -21,14 +21,7 @@ const datosF = async () => {
   return await getStaticProps()
 }
 
-const datos = datosF()
-console.log(datos);
-
 const data = [
-  {
-    name: days[(days.indexOf(day) - 6 + 7) % 7], // Get day of three days ago
-    'Iniciativas creadas': 2780
-  },
   {
     name: days[(days.indexOf(day) - 5 + 7) % 7], // Get day of two days ago
     'Iniciativas creadas': 1890
@@ -44,16 +37,26 @@ const data = [
 ];
 
 export default class Example extends PureComponent {
-  static demoUrl = 'https://codesandbox.io/s/tiny-line-chart-r5z0f';
   render() {
     return (
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart width={300} height={100} data={data}>
-          <Tooltip wrapperStyle={{ outline: 'none' }} contentStyle={{ background: 'transparent', borderRadius: '5px' }} />
-          <Line type="monotone" dataKey="Iniciativas creadas" stroke="#ec4899" strokeWidth={2} />
+      <ResponsiveContainer width="100%" height={200}>
+        <AreaChart
+          width={500}
+          height={200}
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="name" />
           <YAxis />
-        </LineChart>
+          <Tooltip wrapperStyle={{ outline: 'none' }} contentStyle={{ background: '#fff', borderRadius: '5px' }} />
+          <Area type="monotone" dataKey='Iniciativas creadas' stroke="#ec4899" fill="rgb(251 207 232)" />
+        </AreaChart>
       </ResponsiveContainer>
     );
   }
