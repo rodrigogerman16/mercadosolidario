93756bca824f0879da1d4a51324dce8ef7a2e79a
Rodrigo German <rodrigoezequielgerman@gmail.com>
2023-02-05 00:21:27 +0100

    defaultchecked a los checkbox

--- pages/dashboard/usuarios/UsuarioCard.jsx
+++ pages/dashboard/usuarios/UsuarioCard.jsx
@@ -3,15 +3,14 @@ import {BsArrowUpShort, BsArrowDownShort} from "react-icons/bs"
 import axios from "axios";
 
 export default function UsuarioCard({ users }) {
-
   {/*Borrado Logico*/}
   async function handleToggle(user) {
+    console.log(user.isActive)
     await axios.put(`http://localhost:3001/${user.type_of_user}/${user.id}`, {
       isActive: user.isActive ? false : true,
-    });
+    })
+    console.log(user.isActive);
   }
-
-  
   
   const [sortOrder, setSortOrder] = useState('asc');
   const [searchTerm, setSearchTerm] = useState('');  
@@ -96,7 +95,7 @@ export default function UsuarioCard({ users }) {
                       value=""
                       className="sr-only peer"
                       onChange={() => handleToggle(user)}
-                      checked
+                      defaultChecked
                     />
                     <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
                     <span className="ml-3 text-sm font-medium text-gray-900">
