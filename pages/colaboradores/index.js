16a30f46350e275fb17ba026cfb54271e72c1874
Rodrigo German <rodrigoezequielgerman@gmail.com>
2023-02-06 09:14:12 +0100

    typo

--- pages/colaboradores/index.js
+++ pages/colaboradores/index.js
@@ -74,22 +74,13 @@ export default function UsuarioCard({ users }) {
 }
 
 export const getStaticProps = async () => {
-    const company = await fetch(
-      "https://pf-backend-mercadosolidario-production.up.railway.app/company"
-    ).then((res) => res.json());
-  
-    const ong = await fetch(
-      "https://pf-backend-mercadosolidario-production.up.railway.app/ong"
-    ).then((res) => res.json());
   
     const users = await fetch(
-      "https://pf-backend-mercadosolidario-production.up.railway.app/users"
+      "https://pf-backend-mercadosolidario-production.up.railway.app/allusers"
     ).then((res) => res.json());
   
     return {
       props: {
-        company,
-        ong,
         users,
       },
     };
