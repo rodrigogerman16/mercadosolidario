14ed5ff97a1cd1fdcb66a2a629c21c1f2504b219
Rodrigo German <rodrigoezequielgerman@gmail.com>
2023-02-07 07:42:42 +0100

    Ruta para ver iniciativas en las que participa el usuario

---
+++ pages/perfilusuario/iniciativas/index.js
@@ -0,0 +1,11 @@
+import RutaUser from "@/Components/RutaUser";
+import SideBar from "@/Components/SideBar";
+
+export default function Iniciativas() {
+  return (
+    <div>
+      <SideBar />
+      <RutaUser />
+    </div>
+  );
+}
