640d27a2bd2bba255ae287c4e98688f4a2c1ae43
Rodrigo German <rodrigoezequielgerman@gmail.com>
2023-02-07 05:19:17 +0100

    fix typo

--- pages/dashboard/iniciativas/index.js
+++ pages/dashboard/iniciativas/index.js
@@ -83,7 +83,7 @@ const Iniciativas = (props) =>{
           </ul>
         </div>
       </aside>
-          {posts.length && <IniciativasCards posts={posts}/>}
+          {props.posts.length && <IniciativasCards posts={props.posts}/>}
 
         </div>
     )
