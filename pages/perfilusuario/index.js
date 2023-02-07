27d7558c4ab67215f1b4304a58d314e7e79735bb
Rodrigo German <rodrigoezequielgerman@gmail.com>
2023-02-07 07:42:18 +0100

    Diseño y cambios generales

--- pages/perfilusuario/index.js
+++ pages/perfilusuario/index.js
@@ -5,6 +5,7 @@ import axios from "axios";
 import RutaUser from "@/Components/RutaUser";
 import { useUser } from "../../hooks/user";
 import HistorialPerfilUsuario from "../../Components/HistorialPerfilUsuario";
+import SideBar from "@/Components/SideBar";
 export default function Perfilusuario() {
   //.. Estados para el lector de codigo QR
 
@@ -27,20 +28,21 @@ export default function Perfilusuario() {
     birthDate: "",
     profession: "",
   });
-  const [infoUsuario, setInfoUsuario] = useState()
+  console.log(formUsuario)
+  const [infoUsuario, setInfoUsuario] = useState();
 
   useEffect(() => {
-    async function fetchData(){
+    async function fetchData() {
       const usuario = window.localStorage.getItem("user");
-      const usuarioJSON = usuario ? JSON.parse(usuario) : "";
-      const api = await axios(
+      const usuarioJSON = usuario && JSON.parse(usuario);
+      const api = usuarioJSON && await axios(
         "https://pf-backend-mercadosolidario-production.up.railway.app/user/" +
           usuarioJSON.id
       );
 
-      setInfoUsuario(api.data);
-      console.log('actualizando');
-      setFormUsuario({
+      api && setInfoUsuario(api.data);
+      console.log("actualizando");
+      api && setFormUsuario({
         name: api.data.name,
         lastName: api.data.lastName,
         phone: api.data.phone,
@@ -50,8 +52,8 @@ export default function Perfilusuario() {
       });
     }
 
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   const handlerVisible = () => {
     setVisible(visible ? false : true);
@@ -108,90 +110,156 @@ export default function Perfilusuario() {
     return data;
   };
 
-  return (
-    <div className="w-full max-w-md p-4 rounded-md sm:p-8 m-auto min-h-[calc(100vh-100px)] flex flex-col justify-center items-center">
-      <span className="block mb-2 text-xs font-semibold tracking-widest text-center uppercase dark:text-pink-400">
-        Editar mi Perfil
-      </span>
-      <span>Nombre:</span>
-      <input
-        name="name"
-        value={formUsuario.name}
-        onChange={formHandler}
-        required
-      />
-      {errorsForm.name ? <p>{errorsForm.name}</p> : ""}
-      <br />
-      <span>Apellido:</span>
-      <input
-        name="lastName"
-        value={formUsuario.lastName}
-        onChange={formHandler}
-        required
-      />
-      {errorsForm.lastName ? <p>{errorsForm.lastName}</p> : ``}
-      <br />
-      <span>Telefono:</span>
-      <input
-        type="tel"
-        value={formUsuario.phone}
-        name="phone"
-        onChange={formHandler}
-        required
-      />
-      {errorsForm.phone ? <p>{errorsForm.phone}</p> : ""}
-      <br />
-      <span>Linkedin:</span>
-      <input
-        name="user_linkedin"
-        type="url"
-        value={formUsuario.user_linkedin}
-        onChange={formHandler}
-        required
-      />
-      {/* {errorsForm.user_linkedin ? <p>{errorsForm.user_linkedin}</p> : ""} */}
-      <br />
-      <span>Cumpleaños:</span>
-      <input
-        type="date"
-        value={formUsuario.birthDate}
-        name="birthDate"
-        onChange={formHandler}
-        required
-      />
-      {/* {errorsForm.birthDate ? <p>{errorsForm.birthDate}</p> : ""} */}
-      <br />
-      <span>Profesion:</span>
-      <input
-        name="profession"
-        value={formUsuario.profession}
-        onChange={formHandler}
-        required
-      />
-      {/* {errorsForm.profession ? <p>{errorsForm.profession}</p> : ""} */}
-      <button
-        onClick={handlerSubmit}
-        // disabled={
-        //   !formUsuario.name ||
-        //   formUsuario.name.length <= 2 ||
-        //   !formUsuario.lastName ||
-        //   formUsuario.name.length <= 2 ||
-        //   !formUsuario.phone
-        // }
-      >
-        Enviar
-      </button>
+  const profesiones = [
+    { value: "Medico", label: "Medico", checked: false },
+    { value: "Ingeniero", label: "Ingeniero", checked: false },
+    {
+      value: "Profesor",
+      label: "Profesor",
+      checked: false,
+    },
+    {
+      value: "Abogado",
+      label: "Abogado",
+      checked: false,
+    },
+    {
+      value: "Contador",
+      label: "Contador",
+      checked: false,
+    },
+    { value: "Enfermero", label: "Enfermero", checked: false },
+    {
+      value: "Arquitecto",
+      label: "Arquitecto",
+      checked: false,
+    },
+    {
+      value: "Economista",
+      label: "Economista",
+      checked: false,
+    },
+    { value: "Dentista", label: "Dentista", checked: false },
+    { value: "Veterinario", label: "Veterinario", checked: false },
+    {
+      value: "Psicologo",
+      label: "Psicologo",
+      checked: false,
+    },
+    { value: "Farmaceutico", label: "Farmaceutico", checked: false },
+    { value: "Cirujano", label: "Cirujano", checked: false },
+    { value: "Optometrista", label: "Optometrista", checked: false },
+    { value: "Entretenimiento", label: "Entretenimiento", checked: false },
+    {
+      value: "Fisioterapeuta",
+      label: "Fisioterapeuta",
+      checked: false,
+    },
+    { value: "TerapeutaDelHabla", label: "TerapeutaDelHabla", checked: false },
+    {
+      value: "TrabajadorSocial",
+      label: "TrabajadorSocial",
+      checked: false,
+    },
+    { value: "Policia", label: "Policia", checked: false },
+    {
+      value: "Bombero",
+      label: "Bombero",
+      checked: false,
+    },
+    { value: "Militar", label: "Militar", checked: false },
+    { value: "Otros", label: "Otros", checked: false }
+];
 
-      {/* Codigo QR */}
-      <div>
-        <h1>Escanea aqui tu QR</h1>
-        <Html5QrcodePlugin
-          fps={10}
-          qrbox={250}
-          disableFlip={false}
-          qrCodeSuccessCallback={onNewScanResult}
+  return (
+    <div>
+      <SideBar/>
+      {/*Cambiar info */}
+      <h2 className="text-center font-semibold text-2xl mt-10">
+        Editar datos de Perfil
+      </h2>
+      <form
+        className="grid gap-4 mt-16 justify-center items-center"
+        onSubmit={(el) => handlerSubmit(el)}
+      >
+        <div className="flex flex-col">
+          <label className="text-sm">Nombre del titular</label>
+          <input
+            className="rounded w-full border-gray-200 bg-gray-100 p-4 pr-32 text-sm font-medium focus:ring-0 focus:border-gray-200 focus:bg-gray200"
+            type="text"
+            value={formUsuario.name}
+            name={"name"}
+            onChange={formHandler}
+            required
+          />
+          {errorsForm.name ? <p>{errorsForm.name}</p> : ""}
+        </div>
+        <div className="flex flex-col">
+          <label className="text-sm">Apellido del titular</label>
+          <input
+            className="rounded w-full border-gray-200 bg-gray-100 p-4 pr-32 text-sm font-medium focus:ring-0 focus:border-gray-200 focus:bg-gray200"
+            name="lastName"
+            value={formUsuario.lastName}
+            onChange={formHandler}
+            required
+          />
+          {errorsForm.lastName ? <p>{errorsForm.lastName}</p> : ``}
+        </div>
+        <div className="flex flex-col">
+          <label className="text-sm">Telefono</label>
+          <input
+          className="rounded w-full border-gray-200 bg-gray-100 p-4 pr-32 text-sm font-medium focus:ring-0 focus:border-gray-200 focus:bg-gray200"
+            type="tel"
+            value={formUsuario.phone}
+            name="phone"
+            onChange={formHandler}
+            required
+          />
+          {errorsForm.phone ? <p>{errorsForm.phone}</p> : ""}
+        </div>
+        <div className="flex flex-col">
+          <label className="text-sm">Linkedin</label>
+          <input
+          className="rounded w-full border-gray-200 bg-gray-100 p-4 pr-32 text-sm font-medium focus:ring-0 focus:border-gray-200 focus:bg-gray200"
+            name="user_linkedin"
+            type="url"
+            value={formUsuario.user_linkedin}
+            onChange={formHandler}
+            required
+          />
+          {errorsForm.phone ? <label>{errorsForm.phone}</label> : null}
+        </div>
+        <div className="flex flex-col">
+          <label className="text-sm">Fecha de Nacimiento</label>
+          <input
+          className="rounded w-full border-gray-200 bg-gray-100 p-4 pr-32 text-sm font-medium focus:ring-0 focus:border-gray-200 focus:bg-gray200"
+            type="date"
+            value={formUsuario.birthDate}
+            name="birthDate"
+            onChange={formHandler}
+            required
+          />
+          {/* {errorsForm.birthDate ? <p>{errorsForm.birthDate}</p> : ""} */}
+        </div>
+        <div className="flex flex-col">
+          <label className="text-sm">Profesion</label>
+          <select
+          className="rounded w-full border-gray-200 bg-gray-100 p-4 pr-32 text-sm font-medium focus:ring-0 focus:border-gray-200 focus:bg-gray200"
+          type="select"
+          value={formUsuario.profession}
+          name="profession"
+          onChange={formHandler}
+        >
+          {profesiones.map(item => <option value={item.value} key={item.label}>{item.label}</option>)}
+        </select>
+          {/* {errorsForm.profession ? <p>{errorsForm.profession}</p> : ""} */}
+        </div>
+        <input
+          type="submit"
+          value={"Editar"}
+          className="w-full px-8 py-3 font-semibold  bg-black text-white hover:bg-zinc-800 transition-colors rounded my-4 cursor-pointer"
         />
-      </div>
+      </form>
     </div>
   );
 }
