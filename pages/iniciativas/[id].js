95d7b1cce6b53901890178823e7eda71cabc87a2
Delwin183 <90370429+Delwin183@users.noreply.github.com>
2023-02-08 13:56:27 -0400

    create genartor qr

--- pages/iniciativas/[id].js
+++ pages/iniciativas/[id].js
@@ -1,53 +1,66 @@
-"use client"
-import React from 'react'
-import Paypal from '@/Components/Paypal';
-import Image from 'next/image';
-import moment from 'moment/moment';
+"use client";
+import React from "react";
+import Paypal from "@/Components/Paypal";
+import Image from "next/image";
+import moment from "moment/moment";
 
 const Detail = ({ data }) => {
   return (
-    <div className='grid gap-8 max-w-3xl w-full m-auto gap-8 my-8 p-8'>
-      <img className='rounded m-auto w-full aspect-video object-cover' src={data.image} alt={data.title}></img>
-      <div className='flex flex-col gap-4'>
-        <div className='flex items-center justify-between'>
-          <span className="px-2 py-1 rounded bg-pink-400 text-white">{data.type_of_help[0].toUpperCase() + data.type_of_help.slice(1)}</span> {/* UpperCase first letter */}
-          <span className="text-sm dark:text-gray-400">{moment().startOf(data.expirationDate).fromNow()}</span>
+    <div className="grid gap-8 max-w-3xl w-full m-auto gap-8 my-8 p-8">
+      <img
+        className="rounded m-auto w-full aspect-video object-cover"
+        src={data.image}
+        alt={data.title}
+      ></img>
+      <div className="flex flex-col gap-4">
+        <div className="flex items-center justify-between">
+          <span className="px-2 py-1 rounded bg-pink-400 text-white">
+            {data.type_of_help[0].toUpperCase() + data.type_of_help.slice(1)}
+          </span>{" "}
+          {/* UpperCase first letter */}
+          <span className="text-sm dark:text-gray-400">
+            {moment()
+              .startOf(data.expirationDate)
+              .fromNow()}
+          </span>
         </div>
         <h2 className="text-5xl font-bold">{data.title}</h2>
-        <span className='text-gray-400'>📍{data.province}</span>
+        <span className="text-gray-400">📍{data.province}</span>
         <p className="mt-2">{data.description}</p>
+        <div>
+          <div key={item.id}>
+            {qrCodes[item.id] && <img src={qrCodes[item.id]} alt="QR code" />}
+          </div>
+        </div>
         <Paypal postId={data.id}></Paypal>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export async function getStaticPaths() {
-  const res = await fetch("https://pf-backend-mercadosolidario-production.up.railway.app/posts")
-  const data = await res.json()
-  const paths = data.map(({ id }) => ({ params: { id: `${id}` } }))
+  const res = await fetch(
+    "https://pf-backend-mercadosolidario-production.up.railway.app/posts"
+  );
+  const data = await res.json();
+  const paths = data.map(({ id }) => ({ params: { id: `${id}` } }));
   return {
     paths,
-    fallback: false
-  }
+    fallback: false,
+  };
 }
-
-
-
 
 export async function getStaticProps({ params }) {
-  const all = await fetch(`https://pf-backend-mercadosolidario-production.up.railway.app/posts/${params.id}`)
-    .then(res => res.json())
-    .then(data => {
+  const all = await fetch(
+    `https://pf-backend-mercadosolidario-production.up.railway.app/posts/${params.id}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
       return {
         props: {
-          data
-        }
-      }
-    })
-  return all
+          data,
+        },
+      };
+    });
+  return all;
 }
-
-
-
-export default Detail
--- pages/ong/publicaciones/index.js
+++ pages/ong/publicaciones/index.js
@@ -1,58 +1,90 @@
-import { useUser } from "@/hooks/user"
-import Card from "@/Components/Card"
-import Link from "next/link"
+import React, { useState, useEffect } from "react";
+import { useUser } from "@/hooks/user";
+import Card from "@/Components/Card";
+import Link from "next/link";
 
-export default function Publicaciones(props){
-    const userStringify = useUser()
-    const user = userStringify && JSON.parse(userStringify)
-    const userFinded = user && props.ong.filter(item => item.email === user.email)
-    
-    
-    return(
-        <div className="w-3/4 m-auto mt-10">
-            <h2 className="font-bold text-2xl text-center">Mis Iniciativas</h2>
-            <div className="grid w-full col-span-3 grid w-full sm:grid-cols-2 xl:grid-cols-2 gap-4 mt-24">
-            {userFinded && userFinded[0].posts.map(post => (
-              post.isActive &&
-              <Link
-              className="w-full"
-              key={post.id}
-              href={`/iniciativas/${post.id}`}
-              >
-              <Card
-              key={post.id}
-              title={post.title}
-              image={post.image}
-              description={post.description}
-              province={post.province}
-              isVolunteer={post.type_of_help}
-              expirationDate={post.expirationDate}
-            />
-              </Link>
-            ))}
-            </div>
-        </div>
-    )
+import QRCode from "qrcode";
+
+export default function Publicaciones(props) {
+  const userStringify = useUser();
+  const user = userStringify && JSON.parse(userStringify);
+  const userFinded =
+    user && props.ong.filter((item) => item.email === user.email);
+
+  const [qrCode, setQrCode] = useState({});
+
+  const generateQRCode = async (id) => {
+    const code = await QRCode.toDataURL(id);
+    setQrCode(code);
+  };
+
+  return (
+    <div className="w-3/4 m-auto mt-10">
+      <h2 className="font-bold text-2xl text-center">Mis Iniciativas</h2>
+      <div className="grid w-full col-span-3 grid w-full sm:grid-cols-2 xl:grid-cols-3 gap-4 mt-24">
+        {userFinded &&
+          userFinded[0].posts.map(
+            (post) =>
+              post.isActive && (
+                <div>
+                  <Card
+                    key={post.id}
+                    title={post.title}
+                    image={post.image}
+                    description={post.description}
+                    province={post.province}
+                    isVolunteer={post.type_of_help}
+                    expirationDate={post.expirationDate}
+                    render={() => (
+                      <>
+                        <button
+                          onClick={() =>
+                            generateQRCode(post.id, post.type_of_help)
+                          }
+                          type="button"
+                          class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                        >
+                          Generar QR
+                        </button>
+                        {console.log(qrCode)}
+                        {qrCode ? (
+                          <img src={qrCode} />
+                        ) : (
+                          <h3>Error al Generar QR</h3>
+                        )}
+                      </>
+                    )}
+                  />
+                </div>
+              )
+          )}
+      </div>
+    </div>
+  );
 }
 
+export async function getServerSideProps({ req }) {
+  const posts = await fetch(
+    "https://pf-backend-mercadosolidario-production.up.railway.app/posts"
+  ).then((res) => res.json());
 
-export async function getServerSideProps({req}){
-    const posts = await fetch("https://pf-backend-mercadosolidario-production.up.railway.app/posts")
-      .then((res) => res.json())
-  
-    const company = await fetch("https://pf-backend-mercadosolidario-production.up.railway.app/company")
-      .then((res) => res.json())
-  
-    const ong = await fetch("https://pf-backend-mercadosolidario-production.up.railway.app/ong")
-      .then((res) => res.json())
-  
-    const users = await fetch("https://pf-backend-mercadosolidario-production.up.railway.app/allusers")
-      .then((res) => res.json())
-    return{
-      props: {
-        posts,
-        company,
-        ong,
-        users}
-    }
-  }
+  const company = await fetch(
+    "https://pf-backend-mercadosolidario-production.up.railway.app/company"
+  ).then((res) => res.json());
+
+  const ong = await fetch(
+    "https://pf-backend-mercadosolidario-production.up.railway.app/ong"
+  ).then((res) => res.json());
+
+  const users = await fetch(
+    "https://pf-backend-mercadosolidario-production.up.railway.app/allusers"
+  ).then((res) => res.json());
+  return {
+    props: {
+      posts,
+      company,
+      ong,
+      users,
+    },
+  };
+}
